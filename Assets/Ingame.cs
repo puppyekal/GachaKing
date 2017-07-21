bc1b938bc7ba6f7a819cac5322bca02dfae6c47f
agw5256 <agw5256@naver.com>
2017-07-21 17:17:07 +0900

    Ingame변수추가

--- Assets/Ingame.cs
+++ Assets/Ingame.cs
@@ -4,23 +4,72 @@ using UnityEngine;
 
 public class Ingame : MonoBehaviour {
 
-    
+    //방꾸미기 구역
+    public GameObject Area_0;
     public GameObject Area_1;
     public GameObject Area_2;
     public GameObject Area_3;
     public GameObject Area_4;
-    public GameObject Area_5;
-  
+
+    //가챠통
+    public GameObject Gacha_Tong_0;
+    public GameObject Gacha_Tong_1;
+    public GameObject Gacha_Tong_2;
+    public GameObject Gacha_Tong_3;
+    public GameObject Gacha_Tong_4;
+    public GameObject Gacha_Tong_5;
+    public GameObject Gacha_Tong_6;
+    public GameObject Gacha_Tong_7;
+    public GameObject Gacha_Tong_8;
+    public GameObject Gacha_Tong_9;
+
+    //방꾸미기 활성화
+    public bool Area_st = false;
+
+    //가챠통 상태
+    public bool Gacha_Tong_0_st = false;
+    public bool Gacha_Tong_1_st = false;
+    public bool Gacha_Tong_2_st = false;
+    public bool Gacha_Tong_3_st = false;
+    public bool Gacha_Tong_4_st = false;
+    public bool Gacha_Tong_5_st = false;
+    public bool Gacha_Tong_6_st = false;
+    public bool Gacha_Tong_7_st = false;
+    public bool Gacha_Tong_8_st = false;
+    public bool Gacha_Tong_9_st = false;
+
+    //가챠통 10개
+    public int[] Gacha_Initial = new int[10];
+    //가챠통안 10000개
+    public int[,] Gacha_Tong = new int[10, 10000];
+
     // Use this for initialization
     void Start () {
-		
-	}
+        for (int i = 0 ; i < 10; i++) {
+            Initialize(i);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            Gacha_Initial[i] = i;
+        }
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+  
+    }
 
+    //가챠통 초기화
+    public void Initialize(int i) {
+        for (int j = 0; j < 10000; j++)
+        {
+              Gacha_Tong [i, j] = j;
+        }
+      
+    }
+    //버튼
     public void PressKey(int nKey)
     {
         switch (nKey)
@@ -39,6 +88,10 @@ public class Ingame : MonoBehaviour {
                 break;
             case 5:
                 Debug.Log(5);
+                break;
+            case 10000:
+                //방 눌렀을 때
+                Debug.Log(10000);
                 break;
 
         }
