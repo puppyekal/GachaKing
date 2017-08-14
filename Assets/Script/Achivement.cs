7cc4f9ceb31bb03ea4f7c90f48d783c13dbab335
puppyekal <puppyekal@naver.com>
2017-08-15 02:31:25 +0900

    업적 등급 추가

--- Assets/Script/Achivement.cs
+++ Assets/Script/Achivement.cs
@@ -5,39 +5,61 @@ using UnityEngine.UI;
 
 public class Achivement : MonoBehaviour
 {
+    public Text Achivement_Class_Text;
+    public Text Achivement_Class_Information;
+
+
 
     public Text Achivement_EX_text;
     public Image Achivement_EX_Image;
 
     public Text Achivement1;
+    public Image Achivement1_Guage;
     public Text Achivement2;
+    public Image Achivement2_Guage;
     public Text Achivement3;
+    public Image Achivement3_Guage;
     public Text Achivement4;
+    public Image Achivement4_Guage;
     public Text Achivement5;
+    public Image Achivement5_Guage;
     public Text Achivement6;
+    public Image Achivement6_Guage;
     public Text Achivement7;
+    public Image Achivement7_Guage;
     public Text Achivement8;
+    public Image Achivement8_Guage;
     public Text Achivement9;
+    public Image Achivement9_Guage;
     public Text Achivement10;
+    public Image Achivement10_Guage;
     public Text Achivement11;
+    public Image Achivement11_Guage;
     public Text Achivement12;
+    public Image Achivement12_Guage;
     public Text Achivement13;
+    public Image Achivement13_Guage;
     public Text Achivement14;
+    public Image Achivement14_Guage;
     public Text Achivement15;
+    public Image Achivement15_Guage;
+    public Text Achivement16;
+    public Image Achivement16_Guage;
+
+    public static int Achivement_Class = 0;//업적 개수 카운팅
 
     static public int Achivement_EX_cnt = 0;
     // Use this for initialization
     void Start()
     {
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
         Acivement_EX(Ingame.Click_Num);
-        
+
+        Class(Achivement_Class);
     }
     
     public void Acivement_EX(int click)
@@ -53,6 +75,35 @@ public class Achivement : MonoBehaviour
             Debug.Log(Achivement_EX_cnt);
             Achivement_EX_cnt = 1;
         }
+
     }
     
+    public void Class(int temp)
+    {
+        switch(temp)
+        {
+            case 1:
+                Achivement_Class_Text.GetComponent<Text>().text = "늒비";
+                break;
+            case 2:
+                Achivement_Class_Text.GetComponent<Text>().text = "겜잘알";
+                break;
+            case 4:
+                Achivement_Class_Text.GetComponent<Text>().text = "고인물";
+                break;
+            case 8:
+                Achivement_Class_Text.GetComponent<Text>().text = "가챠의 달인";
+                break;
+            case 10:
+                Achivement_Class_Text.GetComponent<Text>().text = "가챠조아";
+                break;
+            case 13:
+                Achivement_Class_Text.GetComponent<Text>().text = "써근물";
+                break;
+            case 16:
+                Achivement_Class_Text.GetComponent<Text>().text = "가챠왕";
+                break;
+
+        }
+    }
 }
