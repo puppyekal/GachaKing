9e066c952b3a2ff861911657781abc109ca8195b
puppyekal <puppyekal@naver.com>
2017-08-16 21:29:21 +0900

    상점 예제 추가

--- Assets/Script/Inventory_Home.cs
+++ Assets/Script/Inventory_Home.cs
@@ -6,19 +6,66 @@ using UnityEngine.UI;
 public class Inventory_Home : MonoBehaviour {
 
     public GameObject Example;
+    public GameObject Item1;
+    public GameObject Item2;
+    public GameObject Item3;
+    public GameObject Item4;
+    public GameObject Item5;
+    public GameObject Item6;
+    public GameObject Item7;
+    public GameObject Item8;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         Example.SetActive(false);
-        Debug.Log(11);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        Item1.SetActive(false);
+        Item2.SetActive(false);
+        Item3.SetActive(false);
+        Item4.SetActive(false);
+        Item5.SetActive(false);
+        Item6.SetActive(false);
+        Item7.SetActive(false);
+        Item8.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
     public void Example_Button()
     {
         Example.SetActive(true);
+    }
+    public void Item1_Button()
+    {
+        Item1.SetActive(true);
+    }
+    public void Item2_Button()
+    {
+        Item2.SetActive(true);
+    }
+    public void Item3_Button()
+    {
+        Item3.SetActive(true);
+    }
+    public void Item4_Button()
+    {
+        Item4.SetActive(true);
+    }
+    public void Item5_Button()
+    {
+        Item5.SetActive(true);
+    }
+    public void Item6_Button()
+    {
+        Item6.SetActive(true);
+    }
+    public void Item7_Button()
+    {
+        Item7.SetActive(true);
+    }
+    public void Item8_Button()
+    {
+        Item8.SetActive(true);
     }
 }
--- Assets/Script/Store_Home.cs
+++ Assets/Script/Store_Home.cs
@@ -5,14 +5,29 @@ using UnityEngine.UI;
 
 public class Store_Home : MonoBehaviour
 {
- //   public GameObject Button_EX;
+    //   public GameObject Button_EX;
     public Button example;
+    public Button item1;
+    public Button item2;
+    public Button item3;
+    public Button item4;
+    public Button item5;
+    public Button item6;
+    public Button item7;
+    public Button item8;
 
     // Use this for initialization
     void Start()
     {
         example.interactable = false;
-        Debug.Log(123);
+        item1.interactable = false;
+        item2.interactable = false;
+        item3.interactable = false;
+        item4.interactable = false;
+        item5.interactable = false;
+        item6.interactable = false;
+        item7.interactable = false;
+        item8.interactable = false;
     }
 
     // Update is called once per frame
@@ -21,6 +36,14 @@ public class Store_Home : MonoBehaviour
         if (Achivement.Achivement_EX_cnt == 1)
         {
             example.interactable = true;
+            item1.interactable = true;
+            item2.interactable = true;
+            item3.interactable = true;
+            item4.interactable = true;
+            item5.interactable = true;
+            item6.interactable = true;
+            item7.interactable = true;
+            item8.interactable = true;
         }
     }
 }
